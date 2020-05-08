23dcdadc7bc19ed933f3ca80e624baec19af7aee
Evgeniy Osipov <osipov2005@yandex.ru>
2020-05-08 16:08:09 +0300

    relax requirements for 'range' and 'nearest k'

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 #include "primitives.h"
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <set>
 
 template <typename T>
 class PointSetTest : public ::testing::Test {
@@ -32,13 +34,28 @@ public:
     {
         auto it = m_set.begin();
         auto end = m_set.end();
-        ASSERT_TRUE(it != end);
+        ASSERT_NE(it, end);
         size_t s = 0;
         while (it != end) {
             ++s;
             ++it;
         }
         ASSERT_EQ(s, correct_size);
+    }
+
+    using iterator_t = typename T::ForwardIt;
+    using set_t = std::set<Point>;
+
+    set_t to_set(std::pair<iterator_t, iterator_t> & range)
+    {
+        set_t res;
+        std::copy(range.first, range.second, std::inserter(res, res.begin()));
+        return res;
+    }
+
+    void contains(const set_t & s, const Point & p)
+    {
+        ASSERT_NE(std::find(s.begin(), s.end(), p), s.end());
     }
 
 	T m_set;
@@ -50,19 +67,19 @@ TYPED_TEST_SUITE(PointSetTest, TestTypes);
 
 TEST(PointSetTest, Point)
 {
-	ASSERT_TRUE(Point(1., 2.) == Point(1., 2.));
-	ASSERT_TRUE(Point(1., 2.) != Point(5., 4.));
-	ASSERT_EQ(Point(0, 0).distance(Point(1, 0)), 1.);
-	ASSERT_EQ(Point(0, 0).distance(Point(0, 1)), 1.);
-	ASSERT_EQ(Point(0, 4).distance(Point(3, 0)), 5.);
+	ASSERT_EQ(Point(1., 2.), Point(1., 2.));
+	ASSERT_NE(Point(1., 2.), Point(5., 4.));
+	ASSERT_DOUBLE_EQ(Point(0, 0).distance(Point(1, 0)), 1.);
+	ASSERT_DOUBLE_EQ(Point(0, 0).distance(Point(0, 1)), 1.);
+	ASSERT_DOUBLE_EQ(Point(0, 4).distance(Point(3, 0)), 5.);
 }
 
 TEST(PointSetTest, Rect)
 {
 	Rect r(Point(1., 1.), Point(2., 2.));
-	ASSERT_EQ(r.distance(Point(1., 1.)), 0.);
-	ASSERT_EQ(r.distance(Point(1.5, 1.5)), 0.);
-	ASSERT_EQ(r.distance(Point(0., 1.)), 1.);
+	ASSERT_DOUBLE_EQ(r.distance(Point(1., 1.)), 0.);
+	ASSERT_DOUBLE_EQ(r.distance(Point(1.5, 1.5)), 0.);
+	ASSERT_DOUBLE_EQ(r.distance(Point(0., 1.)), 1.);
 	ASSERT_TRUE(r.contains(Point(1.5, 1.5)));
 	ASSERT_FALSE(r.contains(Point(.9, 1.5)));
 	ASSERT_TRUE(r.intersects(Rect(Point(0., 0.), Point(1.5, 1.5))));
@@ -86,7 +103,8 @@ TYPED_TEST(PointSetTest, PointSetMethods)
 	ASSERT_FALSE(p.contains(Point(0.5,0)));
 
     auto it = p.begin();
-	ASSERT_TRUE((it->x() == 0.) && (it->y() == 0.));
+	ASSERT_DOUBLE_EQ(it->x(), 0.);
+	ASSERT_DOUBLE_EQ(it->y(), 0.);
 }
 
 TYPED_TEST(PointSetTest, PointSetBasicSearch)
@@ -99,14 +117,12 @@ TYPED_TEST(PointSetTest, PointSetBasicSearch)
 	
 	auto n = p.nearest(Point(.4, .4));
 	ASSERT_TRUE(n.has_value());
-	ASSERT_TRUE(Point(.5, .5) == *n);
+	ASSERT_EQ(Point(.5, .5), *n);
 
 	auto range = p.range(Rect(Point(0.3, 0.3), Point(.7, .7)));
-	auto & it = range.first;
-	auto & out_of_range = range.second;
-	ASSERT_TRUE(*it== Point(.5, .5));
-    ++it;
-	ASSERT_TRUE(it == out_of_range);
+    auto s = this->to_set(range);
+	ASSERT_EQ(s.size(), 1);
+    this->contains(s, Point(.5, .5));
 }
 
 TYPED_TEST(PointSetTest, PointSetNearest0)
@@ -117,7 +133,7 @@ TYPED_TEST(PointSetTest, PointSetNearest0)
 	
 	auto n = p.nearest(Point(.74, .29));
 	ASSERT_TRUE(n.has_value());
-	ASSERT_TRUE(Point(0.725, 0.338) == *n);
+	ASSERT_EQ(Point(0.725, 0.338), *n);
     this->check_size(5);
 }
 
@@ -129,7 +145,7 @@ TYPED_TEST(PointSetTest, PointSetNearest1)
 
     auto n = p.nearest(Point(.712, .567));
 	ASSERT_TRUE(n.has_value());
-	ASSERT_TRUE(Point(0.718, 0.555) == *n);
+	ASSERT_EQ(Point(0.718, 0.555), *n);
     this->check_size(120);
 }
 
@@ -141,17 +157,11 @@ TYPED_TEST(PointSetTest, PointSetRange0)
 
     auto range = p.range(Rect(Point(0.634, 0.276), Point(.818, .42)));
 
-    auto & it = range.first;
-    auto & out_of_range = range.second;
-
-	ASSERT_TRUE(it != out_of_range);
-	ASSERT_TRUE(*it == Point(0.655, 0.382));
-	++it;
-	ASSERT_TRUE(*it == Point(0.725, 0.311));
-	++it;
-	ASSERT_TRUE(*it == Point(0.794, 0.299));
-	++it;
-	ASSERT_TRUE(it == out_of_range); 
+    auto s = this->to_set(range);
+	ASSERT_EQ(s.size(), 3);
+    this->contains(s, Point(0.655, 0.382));
+    this->contains(s, Point(0.725, 0.311));
+    this->contains(s, Point(0.794, 0.299));
     this->check_size(20);
 }
 
@@ -162,16 +172,11 @@ TYPED_TEST(PointSetTest, PointSetNearestK1)
     this->check_size(120);
 
 	auto range = p.nearest(Point(.386, .759), 3);
-    auto & it = range.first;
-    auto & out_of_range = range.second;
 
-	ASSERT_TRUE(it != out_of_range);
-	ASSERT_TRUE(*it == Point(0.376, 0.767));
-	++it;
-	ASSERT_TRUE(*it == Point(0.409, 0.754));
-	++it;
-	ASSERT_TRUE(*it == Point(0.408, 0.728));
-	++it;
-	ASSERT_TRUE(it == out_of_range); 
+    auto s = this->to_set(range);
+	ASSERT_EQ(s.size(), 3);
+    this->contains(s, Point(0.376, 0.767));
+    this->contains(s, Point(0.409, 0.754));
+    this->contains(s, Point(0.408, 0.728));
     this->check_size(120);
 }
